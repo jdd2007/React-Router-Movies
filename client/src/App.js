264d8fd729bc13a4d87850761abd45d30d33e9c6
jacobdurst <jdd2007@gmail.com>
2019-06-10 15:45:09 -0400

    finished project

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -22,8 +23,11 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" render={ (props) => {
+          return (<Movie {...props} addToSavedList={this.addToSavedList}/>)
+        }} />
       </div>
-    );
+    )
   }
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -9,11 +10,17 @@ export default class SavedList extends Component {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
-        <div className="home-button">Home</div>
+        {this.props.list.map( movie => {
+          return (
+            <NavLink to={`/movies/${movie.id}`} key={movie.id} activeClassName="saved-article">
+              <span className="saved-movie">{movie.title}</span>
+            </NavLink>
+          )
+        })}
+        <div className="home-button">
+          <Link to="/">Home</Link>
+        </div>
       </div>
-    );
+    )
   }
 }
